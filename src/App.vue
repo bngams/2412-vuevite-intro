2597b4b4228d7e56444e939521cc3ce966604bcb
bngams <n.boris@devidia.net>
2024-12-09 16:26:09 +0000

    WIP - add components and vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+  import Header from './components/layout/Header.vue';
+  import Welcome from './pages/Welcome.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-app>
+    <v-app-bar :elevation="2"> 
+      <v-app-bar-title>Application Bar</v-app-bar-title>
+    </v-app-bar>
+    <div class="page-container">
+      <Welcome></Welcome>
+    </div>
+  </v-app>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ src/components/layout/Header.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <v-app-bar :elevation="2"> 
+        <v-app-bar-title>Application Bar</v-app-bar-title>
+    </v-app-bar>
+</template>
+
+<style>
+    ul {
+        list-style-type: none;
+        display: flex;
+    } 
+</style>
