1416e8ca622708609e7f59da27fdcede6ecbc59b
bngams <n.boris@devidia.net>
2024-12-10 14:44:24 +0000

    fetch products with pagination

--- src/modules/products/pages/Catalog.vue
+++ src/modules/products/pages/Catalog.vue
@@ -1,14 +1,43 @@
 <script setup lang="ts">
-  import { reactive } from 'vue';
-  import { PRODUCTS_MOCK } from '../mocks/products.mock';
+  import { onMounted, ref } from 'vue';
   import ProductList from '../components/ProductList.vue';
 
-  // ref, reactive
-  const products = reactive(PRODUCTS_MOCK);
+  const isLoading = ref(true);
+  const itemsPerPage = 10;
+  const skip = ref(0);
+
+  // ref, reactive ?? 
+  const products = ref();
+
+  async function fetchProducts() {
+    try { 
+      const response = await fetch(`https://dummyjson.com/products?limit=${itemsPerPage}&skip=${skip.value}`);
+      const data = await response.json();
+      products.value = data.products;
+      isLoading.value = false;
+    } catch(error) {
+      console.log('Error while fetching products', error);
+      alert('Error while fetch products');
+      isLoading.value = false;
+    } 
+  }
+
+  onMounted(fetchProducts)
+
+  function nextPage() {
+    skip.value += itemsPerPage;
+    fetchProducts();
+  } 
 
 </script>
 
 <template>
     <h1>Catalog</h1>
-    <ProductList :products></ProductList>
+    <button @click="nextPage">Page {{ (skip/itemsPerPage) + 1 }}  ></button>
+    <v-progress-linear
+      v-if="isLoading"
+      color="cyan"
+      indeterminate
+    ></v-progress-linear>
+    <ProductList v-else :products></ProductList>
 </template>
