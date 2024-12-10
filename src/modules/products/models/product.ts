2092f523be2f41099b25955e1dd85c92943ae439
bngams <n.boris@devidia.net>
2024-12-10 13:29:51 +0000

    implements products nav and components (catalog, list, card...)

--- src/modules/products/models/product.ts
+++ src/modules/products/models/product.ts
@@ -1,9 +1,11 @@
 export type Product = {
     id: number;
     title: string;
-    description: string;
-    category: string;
+    description?: string;
+    category?: string;
     price: number;
-    thumbnail: string;
-    images: string[];
+    thumbnail?: string;
+    images?: string[];
 };
+
+export type Products = Product[];
--- src/modules/products/pages/Catalog.vue
+++ src/modules/products/pages/Catalog.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
+  import { reactive } from 'vue';
+  import { PRODUCTS_MOCK } from '../mocks/products.mock';
+  import ProductList from '../components/ProductList.vue';
+
+  // ref, reactive
+  const products = reactive(PRODUCTS_MOCK);
+
 </script>
 
 <template>
-    Catalog to display product list and filters
+    <h1>Catalog</h1>
+    <ProductList :products></ProductList>
 </template>
